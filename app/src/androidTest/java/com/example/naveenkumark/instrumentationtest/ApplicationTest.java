754a0545aeb975f8449a30d079113f08b494d104
naveenkumark <naveenkumark@google.com>
2016-09-16 10:57:40 -0700

    updated

--- app/src/androidTest/java/com/example/naveenkumark/instrumentationtest/ApplicationTest.java
+++ app/src/androidTest/java/com/example/naveenkumark/instrumentationtest/ApplicationTest.java
@@ -3,12 +3,15 @@ package com.example.naveenkumark.instrumentationtest;
 import android.app.Application;
 import android.test.ApplicationTestCase;
 
+import com.google.firebase.FirebaseApp;
+
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
+        FirebaseApp.getInstance();
 
     }
 }
